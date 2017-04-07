276269b0bf0517f34cd99e2ceab9ab5cebf3d415
Stephen Morrell <stephen.morrell@gmail.com>
2017-04-07 19:52:28 +0100

    makes jacobians

--- Segmentation.py
+++ Segmentation.py
@@ -31,22 +31,11 @@ def read_file(filename):
   image_array = np.transpose(image_array)
   return image_array.astype('float32')
 
-  # TODO: check image scaling
-# def array_to_image(array):
-#   minimal_value = np.min(array)
-#   maximal_value = np.max(array)
-#   if minimal_value < 0 or maximal_value > 255:
-#     array = 255 * (array - minimal_value) / (maximal_value - minimal_value)
-#   array_uint8 = array.astype('uint8')
-#   return Image.fromarray(array_uint8, 'L')
-
 def save_file(array, filename):
   # coord_transform = np.eye(4)  # TODO: is the transform the same as np.transpose?
   array = np.transpose(array)
   nim = NiftiImage(array)
   nim.save(filename)
-  # imagefile = array_to_image(array)  # TODO: do we need to normalise?
-  # imagefile.save(filename)
 
 def uMRF (pik, k):
   """Calcualtes the Markov Random Field for the whole image for one class k of the central pixel
--- make_priors_and_labels.py
+++ make_priors_and_labels.py
@@ -8,7 +8,7 @@ from pylab import *
 # ref_image = '/home/smorrell/Dropbox/PhD/IPMI/ipmicoursew/MPHGB06_coursework_part1/images/1056_F_71.22_AD_60740.nii'
 import subprocess
 import os
-path = '/home/smorrell/git/ipmicoursew/MPHGB06_coursework_part'
+path = '/home/smorrell/git/ipmi/MPHGB06_coursework_part'
 path_in = path + '1/images/'
 path_ave = path + '2/average/'
 path_out_rigid = path + '1/out_rigid/'
@@ -51,7 +51,7 @@ def resample_priors():
   for file_name in os.listdir(path_in):
     command = 'reg_resample -ref ' + path_in + file_name + ' -flo ' + path_ave + 'average_priors.nii' + \
               ' -res ' + path_out_priors + 'propagated_priors' + file_name + \
-              ' -trans ' + path_out_ff + 'ref_t_flo_new_image_nrr_cpp' + file_name + ' -inter 0 - gpu'  # trans is from f3d
+              ' -trans ' + path_out_ff + 'ref_t_flo_new_image_nrr_cpp' + file_name + ' -inter 3'  # trans is from f3d
 # -trans Filename of the file containing the transformation parametrisation (from reg_aladin, reg_f3d or reg_transform)
     by_my_irroyal(command)
 
@@ -59,8 +59,25 @@ def resample_labels():
   for file_name in os.listdir(path_in):
     command = 'reg_resample -ref ' + path_in + file_name + ' -flo ' + path_ave + 'average_label.nii' + \
               ' -res ' + path_out_labels + 'propagated_labels' + file_name + \
-              ' -trans ' + path_out_ff + 'ref_t_flo_new_image_nrr_cpp' + file_name + ' -inter 0 - gpu'
+              ' -trans ' + path_out_ff + 'ref_t_flo_new_image_nrr_cpp' + file_name + ' -inter 0'
     by_my_irroyal(command)
+
+def make_jacobians():
+  """1 Load the transformations that maps the average image to all other scans
+      2 Generate the Jacobian determinant maps for all subject (using reg_jacobian if using NiftyReg
+       from averge to individual image transforms
+      INPUT 	-trans <filename> 		Filename of the file containing the transformation (mandatory).
+	      -ref <filename>   	Filename of the reference image (required if the transformation is a spline parametrisation)
+      OUTPUT 	-jac <filename> 		Filename of the Jacobian determinant map.
+  """
+  for file_name in os.listdir(path_in):
+    command = 'reg_jacobian -trans '+ path_out_ff + 'ref_t_flo_new_image_nrr_cpp' + file_name + \
+              ' -ref ' + path_in + file_name + \
+              ' -jac ' + path_out_jac + file_name
+    by_my_irroyal(command)
+
+# 3 using the provided average image parcelation, you can compute the average Jacobian determinant values for all
+#   regions of interest
 
 def show_images():
   file_name = os.listdir(path_in)[0]
@@ -83,8 +100,10 @@ def make_difference_images():
 if __name__ == '__main__':
   # linear_registrations()
   # show_images()
-  # non_linear_registration()
-  resample_priors()
+  non_linear_registration()
+  # resample_priors()
+  # resample_labels()
+  make_jacobians()
 
 # dims: ndim, x, y, z, t, u, v, w axis.  reversed.
 # print imgData.filename
